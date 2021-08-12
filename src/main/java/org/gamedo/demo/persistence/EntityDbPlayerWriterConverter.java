82c6bea1091845399539c3e91eb0180d20151af6
pcloves <pcloves@sina.com>
2021-08-12 15:13:54 +0800

    - 持久化是去掉_class字段尝试

--- src/main/java/org/gamedo/demo/persistence/EntityDbPlayerWriterConverter.java
+++ src/main/java/org/gamedo/demo/persistence/EntityDbPlayerWriterConverter.java
@@ -1,8 +1,11 @@
 package org.gamedo.demo.persistence;
 
+import org.bson.Document;
+import org.gamedo.demo.configuration.AppConfig;
 import org.gamedo.persistence.config.MongoConfiguration;
 import org.gamedo.persistence.converter.AbstractEntityDbDataWritingConverter;
 import org.springframework.data.convert.WritingConverter;
+import org.springframework.data.mongodb.core.convert.DefaultMongoTypeMapper;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -10,5 +13,24 @@ import org.springframework.stereotype.Component;
 public class EntityDbPlayerWriterConverter extends AbstractEntityDbDataWritingConverter<EntityDbPlayer> {
     public EntityDbPlayerWriterConverter(MongoConfiguration configuration) {
         super(configuration);
+    }
+
+    @Override
+    public Document convert(EntityDbPlayer source) {
+        final Document document = super.convert(source);
+
+        if (document != null) {
+            document.remove(DefaultMongoTypeMapper.DEFAULT_TYPE_KEY);
+            document.forEach((key, nestedDocument) -> {
+                if (nestedDocument instanceof Document) {
+                    Document componentDbData = (Document) nestedDocument;
+                    if (AppConfig.classMetaIgnoreMap.containsKey(key)) {
+                        componentDbData.remove(DefaultMongoTypeMapper.DEFAULT_TYPE_KEY);
+                    }
+                }
+            });
+        }
+
+        return document;
     }
 }
