2759f4b864b9899cab26135d3f6ff8fa16b9c18f
pcloves <pcloves@sina.com>
2021-08-18 18:29:37 +0800

    Revert "- 持久化是去掉_class字段尝试"
    This reverts commit 82c6bea1091845399539c3e91eb0180d20151af6.

--- src/main/java/org/gamedo/demo/persistence/EntityDbPlayerReadingConverter.java
+++ src/main/java/org/gamedo/demo/persistence/EntityDbPlayerReadingConverter.java
@@ -1,36 +1,14 @@
 package org.gamedo.demo.persistence;
 
-import org.bson.Document;
-import org.gamedo.demo.configuration.AppConfig;
 import org.gamedo.persistence.config.MongoConfiguration;
 import org.gamedo.persistence.converter.AbstractEntityDbDataReadingConverter;
 import org.springframework.data.convert.ReadingConverter;
-import org.springframework.data.mongodb.core.convert.DefaultMongoTypeMapper;
 import org.springframework.stereotype.Component;
 
 @Component
 @ReadingConverter
 public class EntityDbPlayerReadingConverter extends AbstractEntityDbDataReadingConverter<EntityDbPlayer> {
-
     public EntityDbPlayerReadingConverter(MongoConfiguration configuration) {
         super(configuration);
-    }
-
-
-    @Override
-    public EntityDbPlayer convert(Document source) {
-
-        source.put(DefaultMongoTypeMapper.DEFAULT_TYPE_KEY, EntityDbPlayer.class.getName());
-
-        source.forEach((key, nestedDocument) -> {
-            if (nestedDocument instanceof Document) {
-                Document componentDbData = (Document) nestedDocument;
-                if (AppConfig.classMetaIgnoreMap.containsKey(key)) {
-                    componentDbData.put(DefaultMongoTypeMapper.DEFAULT_TYPE_KEY, AppConfig.classMetaIgnoreMap.get(key).getName());
-                }
-            }
-        });
-
-        return super.convert(source);
     }
 }
