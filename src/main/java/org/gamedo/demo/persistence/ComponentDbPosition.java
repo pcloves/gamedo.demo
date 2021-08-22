2ec8984cf579b5aafc32db000fcccb83ce59d95d
zhengtongshan <pcloves@sina.com>
2021-08-22 20:37:20 +0800

    - 添加gamedo-persistence-lombok依赖 - 增加新的profile配置

--- src/main/java/org/gamedo/demo/persistence/ComponentDbPosition.java
+++ src/main/java/org/gamedo/demo/persistence/ComponentDbPosition.java
@@ -3,6 +3,8 @@ package org.gamedo.demo.persistence;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.experimental.Accessors;
+import org.gamedo.annotation.MarkDirty;
 import org.gamedo.persistence.db.ComponentDbData;
 import org.springframework.data.mongodb.core.mapping.Document;
 
@@ -10,6 +12,8 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @Data
 @Document("player")
 @AllArgsConstructor
+@MarkDirty
+@Accessors(chain = true)
 public class ComponentDbPosition extends ComponentDbData
 {
     private int x;
