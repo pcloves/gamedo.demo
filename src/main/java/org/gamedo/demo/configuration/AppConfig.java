36bf74e4029c17777720dc3a4fb45c24ba9bc273
pcloves <pcloves@sina.com>
2021-09-02 13:13:37 +0800

    - 升级依赖：gamedo-persistence-spring-boot-starter

--- src/main/java/org/gamedo/demo/configuration/AppConfig.java
+++ src/main/java/org/gamedo/demo/configuration/AppConfig.java
@@ -1,22 +1,7 @@
 package org.gamedo.demo.configuration;
 
-import org.gamedo.persistence.GamedoMongoTemplate;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.mongodb.MongoDatabaseFactory;
-import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.data.mongodb.core.convert.MongoConverter;
 
 @Configuration
 public class AppConfig {
-
-    @Bean
-    public MongoTemplate mongoTemplate(final MongoDatabaseFactory databaseFactory, final MongoConverter mongoConverter) {
-        return new MongoTemplate(databaseFactory, mongoConverter);
-    }
-
-    @Bean
-    public GamedoMongoTemplate gamedoMongoTemplate(MongoTemplate mongoTemplate) {
-        return new GamedoMongoTemplate(mongoTemplate);
-    }
 }
