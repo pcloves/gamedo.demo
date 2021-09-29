da44235bd90ab479c225f3e1379fbafeeb09f65d
pcloves <pcloves@sina.com>
2021-09-29 17:50:26 +0800

    - 升级依赖：gamedo-spring-boot-starter

--- src/main/java/org/gamedo/demo/configuration/DemoApp.java
+++ src/main/java/org/gamedo/demo/configuration/DemoApp.java
@@ -3,6 +3,7 @@ package org.gamedo.demo.configuration;
 import org.gamedo.Gamedo;
 import org.gamedo.configuration.GameLoopProperties;
 import org.gamedo.configuration.GamedoProperties;
+import org.gamedo.gameloop.GameLoop;
 import org.gamedo.gameloop.GameLoopConfig;
 import org.gamedo.gameloop.interfaces.IGameLoopGroup;
 import org.springframework.context.ApplicationContext;
@@ -14,13 +15,13 @@ import java.util.concurrent.atomic.AtomicInteger;
 public class DemoApp extends Gamedo {
 
     @SuppressWarnings({"NonFinalStaticVariableUsedInClassInitialization"})
-    private static final class MyHolder
-    {
+    private static final class MyHolder {
         public static final IGameLoopGroup db = applicationContext.getBean(IGameLoopGroup.class, GameLoopConfig.builder()
                 .gameLoopGroupId("dbs")
                 .gameLoopCount(Runtime.getRuntime().availableProcessors() * 10)
                 .gameLoopIdPrefix("db-")
                 .gameLoopIdCounter(new AtomicInteger(1))
+                .gameLoopImplClazz(GameLoop.class)
                 .componentRegisters(GameLoopConfig.DEFAULT.getComponentRegisters())
                 .daemon(false)
                 .build()
